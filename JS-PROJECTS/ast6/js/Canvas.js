75fbeb75876537d3734c2593d61030aecc547a53
Govinda Bhandari <govinda.bhandari744@gmail.com>
2019-12-04 22:38:35 +0545

    helix

--- JS-PROJECTS/ast6/js/Canvas.js
+++ JS-PROJECTS/ast6/js/Canvas.js
@@ -7,11 +7,13 @@ class Canvas {
 
     MIN_CIRCLE_RADIUS = 1;
     MAX_CIRCLE_RADIUS = 5;
-    NUMBER_OF_ROWS = 10;
-    NUMBER_OF_COLUMNS = 10;
+    NUMBER_OF_ROWS = 12;
+    NUMBER_OF_COLUMNS = 15;
     NUMBER_OF_STRANDS = 2;
     frameCount = 0;
     phase = 0;
+
+    ROTATE_POINT = 7;
 
     CIRCLE_FLICKER_RATE = 0.1;
 
@@ -19,9 +21,9 @@ class Canvas {
 
     helixFrom = 3;  //3rd of height of canvas
 
-    offSetX = 30;
+    offSetX = 20;
 
-    SPEED_OF_ROTATION = 0.03;
+    SPEED_OF_ROTATION = 0.02;
 
 
     constructor(height, width, parentClass) {
@@ -35,6 +37,8 @@ class Canvas {
 
         this.canvasElement.height = this.height;
         this.canvasElement.width = this.width;
+
+        this.canvasElement.style.background = '#3f4b61';
 
         this.canvasElement.style.margin = '0 auto';
 
@@ -63,7 +67,7 @@ class Canvas {
                 centerX = 0;
                 for(let cols = 0; cols < this.NUMBER_OF_COLUMNS; cols++) {
                     centerX += this.offSetX;
-                    columnOffset = (cols * this.NUMBER_OF_STRANDS * Math.PI) / this.NUMBER_OF_COLUMNS;
+                    columnOffset = (cols * this.NUMBER_OF_STRANDS) / this.ROTATE_POINT;
 
                     for(let rows = 0; rows < this.NUMBER_OF_ROWS; rows++) {
                         centerY = this.height / this.helixFrom + rows * this.NUMBER_OF_ROWS + Math.sin(strandPhase + columnOffset) * this.STRAND_ROTATE_SPACE;
@@ -71,7 +75,7 @@ class Canvas {
                         let circleRadius = sizeOffset * this.MAX_CIRCLE_RADIUS;
 
                         this.canvasContext.beginPath();
-                        this.canvasContext.arc(centerX, centerY, circleRadius, 0, 2 * Math.PI);
+                        this.canvasContext.arc(centerX, centerY, circleRadius, 0, this.endAngle);
                         var my_gradient = this.canvasContext.createLinearGradient(0, 0, 0, 400);
                         my_gradient.addColorStop(0, "yellow");
                         my_gradient.addColorStop(1, "green");
